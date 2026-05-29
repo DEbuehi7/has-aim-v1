e4b9973cb080ecb0b186a121ed39dc7ef0f3ec42
DEbuehi7 <edoaim2030@pm.me>
2026-05-29 16:12:23 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,23 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
- typescript: { ignoreBuildErrors: true },
+  typescript: { ignoreBuildErrors: true },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/aura8",
+        permanent: false,
+        has: [{ type: "host", value: "aura8.fun" }],
+      },
+      {
+        source: "/",
+        destination: "/aura8",
+        permanent: false,
+        has: [{ type: "host", value: "www.aura8.fun" }],
+      },
+    ];
+  },
 };
+
 export default nextConfig;
