b05f145432e65a4aab59ebd66fe1399f2d9b515b
DEbuehi7 <edoaim2030@pm.me>
2026-06-03 21:01:19 -0700

    Update page.tsx

--- app/aura8/privacy/page.tsx
+++ app/aura8/privacy/page.tsx
@@ -159,9 +159,11 @@ export default function Aura8Privacy() {
         textAlign: "center",
       }}>
         <div style={{ fontSize: "9px", color: "#3F3F46", letterSpacing: "0.1em" }}>
-          AURA8 -- SMILING BUBBLES INC. -- ADULTS 18+ ONLY --{" "}
-          <a href="/aura8/terms" style={{ color: "#52525B", textDecoration: "none" }}>TERMS OF SERVICE</a>
-        </div>
+  AURA8 -- SMILING BUBBLES INC. -- ADULTS 18+ ONLY --{" "}
+  <a href="/aura8/terms" style={{ color: "#52525B", textDecoration: "none" }}>TERMS</a>
+  {" "}--{" "}
+  <a href="/aura8/acceptable-use" style={{ color: "#52525B", textDecoration: "none" }}>ACCEPTABLE USE</a>
+</div>
       </div>
     </div>
   );
